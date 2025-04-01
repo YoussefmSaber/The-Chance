b2d10ac28de2ee04c01ebd5b7a24ab5b4bb4b58a
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-01 17:15:51 +0200

    Created the test cases for the sudoku checker for 4*4, 9*9 or 16*16 puzzle

--- src/week1/sudoku_checker/Main.kt
+++ src/week1/sudoku_checker/Main.kt
@@ -3,3 +3,8 @@ package week1.sudoku_checker
 fun main() {
     print("hello hello")
 }
+
+fun sudokuChecker(puzzle: List<List<Char>>): Boolean {
+
+    return false
+}
--- src/week1/sudoku_checker/TestCases.kt
+++ src/week1/sudoku_checker/TestCases.kt
@@ -1,5 +1,18 @@
 package week1.sudoku_checker
 
-fun main () {
-    println("hello test world")
+import utils.TestCaseVariables
+import utils.test
+
+fun main() {
+    sudokuTestCases()
+}
+
+fun sudokuTestCases() {
+    TestCaseVariables.SudokuTestCases.forEach { sudokuPuzzle ->
+        test(
+            name = sudokuPuzzle.name,
+            result = sudokuChecker(sudokuPuzzle.puzzle),
+            correctResult = sudokuPuzzle.isSolvable
+        )
+    }
 }
